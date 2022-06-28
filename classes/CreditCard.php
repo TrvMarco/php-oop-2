32983f5d3a5bc708b8383f5d1843fa1e343ef530
TrvMarco <trevisanello.marco@gmail.com>
2022-06-28 17:59:54 +0200

    Creato file

---
+++ classes/CreditCard.php
@@ -0,0 +1,10 @@
+<?php
+
+class CreditCard {
+    private $number;
+    private $expire_date;
+    private $brand;
+    private $name;
+    private $surname;
+    private $cvc;
+}
---
+++ classes/user.php
@@ -0,0 +1,9 @@
+<?php
+
+class User{
+    public $name;
+    public $surname;
+    public $email;
+    public $discount;
+    private $credit_card;
+}
